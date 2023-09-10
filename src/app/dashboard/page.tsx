dd347f3799418c7d180a9870aecf61bbf621fa72
andrea <zappavignandrea@gmail.com>
2023-09-10 16:13:05 +0200

    initial setup

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import Button from "@/components/ui/Button";
+
+interface DashboardProps {}
+
+const Dashboard: FC<DashboardProps> = ({}) => {
+  return <Button>hello</Button>;
+};
+
+export default Dashboard;
