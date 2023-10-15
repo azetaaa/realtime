0cc5dae645a1fe024182c76566f19c0e1c7da694
andrea <zappavignandrea@gmail.com>
2023-10-15 12:15:28 +0200

    add message functionality

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,7 +12,7 @@ import { SidebarOption } from "@/types/typings";
 import { Icon, Icons } from "@/components/Icons";
 // import MobileChatLayout from "@/components/MobileChatLayout";
 import SidebarFriendRequests from "@/components/SidebarFriendRequests";
-// import SidebarChatList from "@/components/SidebarChatList";
+import SidebarChatList from "@/components/SidebarChatList";
 import SignOutButton from "@/components/SignOutButton";
 
 interface LayoutProps {
@@ -33,7 +33,7 @@ const Layout = async ({ children }: LayoutProps) => {
   if (!session) notFound();
 
   const friends = await getFriendsByUserId(session.user.id);
-  console.log("friends", friends);
+  // console.log("friends", friends);
 
   const unseenRequestCount = (
     (await fetchRedis(
@@ -49,45 +49,49 @@ const Layout = async ({ children }: LayoutProps) => {
           <Icons.Logo className="h-8 w-auto text-indigo-600" />
         </Link>
 
-        <div className="text-xs font-semibold leading-6 text-gray-400">
-          Your chats
-        </div>
+        {friends.length > 0 ? (
+          <div className="text-xs font-semibold leading-6 text-gray-400">
+            Your chats
+          </div>
+        ) : null}
 
         <nav className="flex flex-1 flex-col">
           <ul role="list" className="flex flex-1 flex-col gap-y-7">
-            <li> * chats that this user has * </li>
+            <li>
+              <SidebarChatList sessionId={session.user.id} friends={friends} />
+            </li>
             <li>
               <div className="text-xs font-semibold leading-6 text-gray-400">
                 Overview
               </div>
+
+              <ul role="list" className="-mx-2 mt-2 space-y-1">
+                {sidebarOptions.map((option) => {
+                  const Icon = Icons[option.Icon];
+                  return (
+                    <li key={option.id}>
+                      <Link
+                        href={option.href}
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                      >
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
+                          <Icon className="h-4 w-4" />
+                        </span>
+
+                        <span className="truncate">{option.name}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+
+                <li>
+                  <SidebarFriendRequests
+                    sessionId={session.user.id}
+                    initialUnseenRequestCount={unseenRequestCount}
+                  />
+                </li>
+              </ul>
             </li>
-
-            <ul role="list" className="-mx-2 mt-2 space-y-1">
-              {sidebarOptions.map((option) => {
-                const Icon = Icons[option.Icon];
-                return (
-                  <li key={option.id}>
-                    <Link
-                      href={option.href}
-                      className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                    >
-                      <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
-                        <Icon className="h-4 w-4" />
-                      </span>
-
-                      <span className="truncate">{option.name}</span>
-                    </Link>
-                  </li>
-                );
-              })}
-
-              <li>
-                <SidebarFriendRequests
-                  sessionId={session.user.id}
-                  initialUnseenRequestCount={unseenRequestCount}
-                />
-              </li>
-            </ul>
 
             <li className="-mx-6 mt-auto flex items-center">
               <div className="flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900">
